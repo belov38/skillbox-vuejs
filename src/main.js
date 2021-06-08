5eaf351f2e76061f67a8148a072458a5982893f6
Ilia Belov <belov38@gmail.com>
2021-06-08 18:44:06 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-
-import {message1,message2} from "./data"
-import ShowMessage from "./msg.js"
-
-ShowMessage(message1);
-ShowMessage(message2);
-
 Vue.config.productionTip = false;
 
 new Vue({
