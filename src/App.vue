2eb95982d19e35d94bf7c05ba982828920216cee
Ilia Belov <belov38@gmail.com>
2021-06-10 11:51:37 +1200

    router-vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,9 @@
 <template>
-  <component :is="currentPageComponent" :page-params="currentPageParams"/>
+  <router-view />
 </template>
 
 <script>
-import MainPage from "./pages/MainPage.vue"
-import ProductPage from "./pages/ProductPage.vue"
-import NotFoundPage from "./pages/NotFoundPage.vue"
-import eventBus from "@/eventBus.js"
-
-const routes={
-  main:'MainPage',
-  product:'ProductPage',
-
-}
 export default {
-  name: "App",  
-  components: {MainPage, ProductPage, NotFoundPage},
-  methods:{
-    gotoPage(pageName, pageParams){
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    }
-  },
-  data() {
-    return {     
-      currentPage: 'main',
-      currentPageParams: {}
-    };
-  }, 
-  computed:{
-    currentPageComponent() {
-      return routes[this.currentPage] || 'NotFoundPage'    
-    }
-  },
-  created(){
-    eventBus.$on('gotoPage',(pageName, pageParams)=>this.gotoPage(pageName, pageParams))
-  }
+
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MainPage from '@/pages/MainPage.vue'
+import NotFoundPage from '@/pages/NotFoundPage.vue'
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -8,7 +10,13 @@ const routes = [
         name: 'main',
         component: MainPage,
         path: '/'
-    }
+    },
+    {
+        name: 'notFound',
+        component: NotFoundPage,
+        path: '*'
+    },
+
 ];
 const router = new VueRouter({ routes });
 
