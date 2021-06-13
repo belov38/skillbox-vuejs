735b6f2c330cdd008962e3f629252e07f4958866
Ilia Belov <belov38@gmail.com>
2021-06-13 14:28:47 +1200

    OrderList component

---
+++ src/components/OrderList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul class="cart__orders">
+      <li class="cart__order" v-for="i in cartProducts" :key=i.id>
+        <h3>{{ i.product.title }}</h3>
+        <b>{{ i.price | numberFormat}} ₽</b>
+        <span>Артикул: {{ i.product.id  }}</span>
+      </li>
+    </ul>
+
+    <div class="cart__total">
+      <p>Доставка: <b>500 ₽</b></p>
+      <p>Итого: <b>{{ cartProducts.length }}</b> товара на сумму <b>{{ totalPrice+500 | numberFormat }} ₽</b></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import numberFormat from "@/helpers/numberFormat";
+
+
+export default {
+    filters: { numberFormat },
+    props:['cartProducts','totalPrice'] 
+};
+</script>
--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -82,29 +82,7 @@
         </div>
 
         <div class="cart__block">
-          <ul class="cart__orders">
-            <li class="cart__order">
-              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
-              <b>18 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
-              <b>4 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
-              <b>8 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-          </ul>
-          
-          <div class="cart__total">
-            <p>Доставка: <b>500 ₽</b></p>
-            <p>Итого: <b>3</b> товара на сумму <b>37 970 ₽</b></p>
-          </div>
-
+          <OrderList :cartProducts="cartProducts" :totalPrice="totalPrice" />
           <button class="cart__button button button--primery" type="submit">
             Оформить заказ
           </button>
@@ -123,11 +101,20 @@
 <script>
 import BaseFormText from "@/components/BaseFormText.vue"
 import BaseFormTextarea from "@/components/BaseFormTextarea.vue"
+import OrderList from "@/components/OrderList.vue";
+import { mapGetters } from "vuex";
 import axios from 'axios'
 import { API_BASE_URL } from "@/config"
 
 export default {
-    components:{ BaseFormText, BaseFormTextarea },
+    components:{ BaseFormText, BaseFormTextarea, OrderList },
+    computed: {
+      ...mapGetters({
+      totalPrice: "cartTotalPrice",
+      cartProducts: "cartProducts",      
+    })
+    },
+
     data(){
         return{
             formData:{},
