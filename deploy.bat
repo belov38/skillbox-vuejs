0ff88d33bd764c891bc2e77a0a6784f076b7077b
Ilia Belov <belov38@gmail.com>
2021-06-16 13:59:37 +1200

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,10 @@
 call npm run build
 echo pushing
 
+rd /s /q ./dist
+mkdir ./dist
+
 cd ./dist
-git init
 git add -A
 git commit -m 'deploy'
 git push -f git push -f git@github.com:belov38/skillbox-vue-deploy.git master:gh-pages
